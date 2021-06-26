55189b48f38ae9b64656a418df02155836ff2d35
Khadija Boudinar <khadija.boudinar@esprit.tn>
2021-06-26 12:29:20 +0100

    initialuser

---
+++ src/main/java/com/esprit/springjwt/User_AuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class User_AuthenticationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(User_AuthenticationApplication.class, args);
+}
+}
